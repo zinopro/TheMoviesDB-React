5cd9ae04c26134a5bee1a28ad419292cef7c5eea
eliz <eliz@codeoasis.com>
2022-04-06 14:02:49 -0500

    add page not found component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import MovieDetail from './pages/MovieDetail';
+import PageNotFound from './pages/PageNotFound';
 import { CSSStyle } from './global-style';
 
 
@@ -9,9 +10,12 @@ const App = () => (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/:movieId' element={<MovieDetail />} />
+      <Route path='/*' element={<PageNotFound />} />
     </Routes>
     <CSSStyle />
   </Router>
 );
 
 export default App;
+
+
